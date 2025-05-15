ab84dd9c301523ca8b934cde444d3d517bafb7de
_Kentoyyy <69066165+Kentoyyy@users.noreply.github.com>
2025-05-15 11:27:33 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app.mount("/audio", StaticFiles(directory=os.path.join(os.getcwd(), "audio/corre
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://early-edge.vercel.app/"],  # Update this for production
+    allow_origins=["https://early-edge.vercel.app"],  # Update this for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
